787d771732311edc13c902b0ee7de967b5e7f5ea
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2025-02-25 12:04:32 +0530

    baud_generator.v

--- uart/baud_generator.v
+++ uart/baud_generator.v
@@ -1,32 +1,35 @@
 module baud_generator (
     input wire clk,      
-    output reg rx_en,  
-    output reg tx_en    
+    output wire rx_en,  
+    output wire tx_en    
 );
 
-integer RX_MAX = 50000000 / (115200 * 16);
-integer TX_MAX = 50000000 / 115200;      
+parameter RX_MAX = 50000000 / (115200 * 16); // Oversampling 16x
+parameter TX_MAX = 50000000 / 115200;
 
-reg [4:0] rx_count = 0; 
-reg [8:0] tx_count = 0; 
+reg [15:0] rx_count = 0;
+reg [15:0] tx_count = 0;
+reg rx_en_reg = 0;
+reg tx_en_reg = 0;
+
+assign rx_en = rx_en_reg;
+assign tx_en = tx_en_reg;
 
 always @(posedge clk) begin
     if (rx_count == RX_MAX - 1) begin
         rx_count <= 0;
-        rx_en <= 1; 
+        rx_en_reg <= 1;
     end else begin
         rx_count <= rx_count + 1;
-        rx_en <= 0;
+        rx_en_reg <= 0;
     end
-end
 
-always @(posedge clk) begin
     if (tx_count == TX_MAX - 1) begin
         tx_count <= 0;
-        tx_en <= 1;
+        tx_en_reg <= 1;
     end else begin
         tx_count <= tx_count + 1;
-        tx_en <= 0;
+        tx_en_reg <= 0;
     end
 end
 
