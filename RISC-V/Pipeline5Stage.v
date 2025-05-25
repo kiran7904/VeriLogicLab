c6187fc5ca3f7e95f4937dc0145be2e70674ad47
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2025-05-25 12:12:34 +0530

    Pipeline5Stage.v

--- RISC-V/Pipeline5Stage.v
+++ RISC-V/Pipeline5Stage.v
@@ -4,16 +4,43 @@ module Pipeline5Stage(
 );
 
 reg [31:0] pc;
-wire [31:0] instruction, pc_out;
+wire [31:0] pc_out, instruction;
 wire [4:0] rs1, rs2, rd;
-wire [31:0] imm, read_data1, read_data2, alu_result, mem_out, wb_data;
+wire [31:0] imm, read_data1, read_data2;
+wire [31:0] alu_result, mem_out, wb_data;
 
-IF_stage IF(clk, reset, pc, pc_out, instruction);
-ID_stage ID(clk, reset, instruction, rs1, rs2, rd, imm);
-RegFile RF(clk, rs1, rs2, rd, wb_data, 1'b1, read_data1, read_data2);
-EX_stage EX(read_data1, imm, alu_result);
-MEM_stage MEM(alu_result, mem_out);
-WB_stage WB(mem_out, wb_data);
+IF_stage IF(
+    .clk(clk), .reset(reset),
+    .pc_in(pc), .pc_out(pc_out),
+    .instruction(instruction)
+);
+
+ID_stage ID(
+    .instruction(instruction),
+    .rs1(rs1), .rs2(rs2), .rd(rd), .imm(imm)
+);
+
+RegFile RF(
+    .clk(clk),
+    .rs1(rs1), .rs2(rs2), .rd(rd),
+    .write_data(wb_data), .reg_write(1'b1),
+    .read_data1(read_data1), .read_data2(read_data2)
+);
+
+EX_stage EX(
+    .read_data1(read_data1), .imm(imm),
+    .alu_result(alu_result)
+);
+
+MEM_stage MEM(
+    .alu_result(alu_result),
+    .mem_out(mem_out)
+);
+
+WB_stage WB(
+    .mem_out(mem_out),
+    .wb_data(wb_data)
+);
 
 always @(posedge clk or posedge reset) begin
     if (reset)
