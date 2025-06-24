927e5a68ec8efaff379446bc8f88bb489da8e983
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2025-06-24 12:08:49 +0530

    Update uart_top.v

--- uart/uart_top.v
+++ uart/uart_top.v
@@ -1,23 +1,23 @@
-module uart_top (
+module uart_loopback_top (
     input wire clk,
-    input wire rx,
-    input wire [7:0] data_in,
     input wire send,
-    output wire tx,
-    output wire busy,
+    input wire [7:0] data_in,
+    input wire clr_ready,
     output wire [7:0] data_out,
     output wire data_ready
 );
 
-wire rx_en, tx_en;
+wire tx;        
+wire tx_en, rx_en;
+wire busy;
 
+// Instantiate baud generator
 baud_generator baud_gen (
     .clk(clk),
     .rx_en(rx_en),
     .tx_en(tx_en)
 );
-
-uart_tx transmitter (
+uart_tx tx_inst (
     .clk(clk),
     .tx_en(tx_en),
     .data_in(data_in),
@@ -26,11 +26,12 @@ uart_tx transmitter (
     .busy(busy)
 );
 
-uart_rx receiver (
+// Receiver
+uart_rx rx_inst (
     .clk(clk),
-    .rx(rx),
+    .rx(tx),          
     .rx_en(rx_en),
-    .clr_ready(send),
+    .clr_ready(clr_ready),
     .data_out(data_out),
     .data_ready(data_ready)
 );
