bc08364fe6140409ae2bb1bdf3f11fbc0b7c2fb6
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2025-08-03 10:54:57 +0530

    Update module i2c_slave.v

--- I2C/module i2c_slave.v
+++ I2C/module i2c_slave.v
@@ -6,16 +6,13 @@ module i2c_slave #(
     inout sda,
     input scl,
     output reg [7:0] received_data,
-    input [7:0] send_data,
-    input [7:0] id
+    input [7:0] send_data
 );
 
+    reg [2:0] state;
     reg [3:0] bit_cnt;
     reg [7:0] shift_reg;
-    reg ack;
-    reg sda_out;
-    reg sda_en;
-    reg [2:0] state;
+    reg sda_out, sda_en;
 
     assign sda = sda_en ? sda_out : 1'bz;
 
@@ -23,45 +20,44 @@ module i2c_slave #(
         if (rst) begin
             state <= 0;
             bit_cnt <= 0;
-            received_data <= 0;
             shift_reg <= 0;
+            received_data <= 0;
             sda_en <= 0;
             sda_out <= 1;
         end else begin
             case (state)
-                0: begin
-                    shift_reg[7-bit_cnt] <= sda;
-                    if (bit_cnt == 6) state <= 1;
-                    else bit_cnt <= bit_cnt + 1;
-                end
 
-                1: begin
-                    shift_reg[0] <= sda;
-                    bit_cnt <= 0;
-                    if (shift_reg[7:1] == SLAVE_ADDR) begin
-                        ack <= 1;
-                        state <= (sda == 1'b0) ? 2 : 3;
-                    end else begin
-                        ack <= 0;
-                        state <= 7;
+                0: begin
+                    shift_reg[7 - bit_cnt] <= sda;
+                    bit_cnt <= bit_cnt + 1;
+                    if (bit_cnt == 7) begin
+                        state <= (shift_reg[7:1] == SLAVE_ADDR) ? (sda ? 3 : 2) : 7;
+                        bit_cnt <= 0;
                     end
                 end
 
+
                 2: begin
-                    shift_reg[7-bit_cnt] <= sda;
+                    shift_reg[7 - bit_cnt] <= sda;
+                    bit_cnt <= bit_cnt + 1;
                     if (bit_cnt == 7) begin
                         received_data <= shift_reg;
                         state <= 4;
-                    end else
-                        bit_cnt <= bit_cnt + 1;
+                        bit_cnt <= 0;
+                    end
                 end
+
 
                 3: begin
                     sda_en <= 1;
                     sda_out <= send_data[7 - bit_cnt];
-                    if (bit_cnt == 7) state <= 6;
-                    else bit_cnt <= bit_cnt + 1;
+                    bit_cnt <= bit_cnt + 1;
+                    if (bit_cnt == 7) begin
+                        state <= 6;
+                        bit_cnt <= 0;
+                    end
                 end
+
 
                 4: begin
                     sda_en <= 1;
@@ -69,10 +65,12 @@ module i2c_slave #(
                     state <= 7;
                 end
 
+
                 6: begin
                     sda_en <= 0;
                     state <= 7;
                 end
+
 
                 7: begin
                     sda_en <= 0;
