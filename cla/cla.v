d985b3f8ece9e9ac73a90342c07a5cc36711ed3d
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2025-09-29 21:37:34 +0530

    Create cla.v

---
+++ cla/cla.v
@@ -0,0 +1,23 @@
+module cla_4bit (
+  input  [3:0] a, b,
+  input        cin,
+  output [3:0] sum,
+  output       cout
+);
+  wire [4:0] c;
+  wire [3:0] p, g;
+
+  assign c[0] = cin;
+
+  genvar i;
+  generate
+    for (i = 0; i < 4; i = i + 1) begin
+      assign p[i]   = a[i] ^ b[i];
+      assign g[i]   = a[i] & b[i];
+      assign c[i+1] = g[i] | (p[i] & c[i]);
+      assign sum[i] = p[i] ^ c[i];
+    end
+  endgenerate
+
+  assign cout = c[4];
+endmodule
