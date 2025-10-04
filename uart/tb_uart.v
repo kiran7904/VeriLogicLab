1f0e1ab9cf245b0a59e4f29e5c03a88af4479d21
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2025-10-04 10:30:59 +0530

    Update tb_uart.v

--- uart/tb_uart.v
+++ uart/tb_uart.v
@@ -17,28 +17,27 @@ uart_top uut (
     .data_ready(data_ready)
 );
 
-always #10 clk = ~clk; // 50 MHz clock
+always #10 clk = ~clk;
 
 initial begin
     $monitor("Time=%0t | rx=%b | tx=%b | busy=%b | data_ready=%b | data_out=%h", 
              $time, rx, tx, busy, data_ready, data_out);
 
     #100;
-    data_in = 8'hA5;  
+    data_in = 8'hA5;
     send = 1;
     #20 send = 0;
 
-    //  'A5' = 10100101
-    #8680 rx = 0; // Start bit
-    #8680 rx = 1; // Bit 0
-    #8680 rx = 0; // Bit 1
-    #8680 rx = 1; // Bit 2
-    #8680 rx = 0; // Bit 3
-    #8680 rx = 0; // Bit 4
-    #8680 rx = 1; // Bit 5
-    #8680 rx = 0; // Bit 6
-    #8680 rx = 1; // Bit 7
-    #8680 rx = 1; // Stop bit
+    #8680 rx = 0;
+    #8680 rx = 1;
+    #8680 rx = 0;
+    #8680 rx = 1;
+    #8680 rx = 0;
+    #8680 rx = 0;
+    #8680 rx = 1;
+    #8680 rx = 0;
+    #8680 rx = 1;
+    #8680 rx = 1;
 
     #100000;
     $stop;
